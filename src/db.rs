65edec9daa821912de0ccac56aba71652f9c80c4
Akbar Rahman <hi@alv.cx>
2024-07-17 02:06:53 +0100

    fix compose files issues

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,9 @@
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
-use dotenvy::dotenv;
 use std::env;
 
 pub fn establish_connection() -> PgConnection {
     let database_url = env::var("DERPIC_DATABASE_URL").expect("DERPIC_DATABASE_URL must be set");
     PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+        .unwrap_or_else(|e| panic!("Error connecting to {}: {e}", database_url))
 }
